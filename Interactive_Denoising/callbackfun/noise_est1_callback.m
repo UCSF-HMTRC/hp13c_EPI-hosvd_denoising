98752d90d5682ac7c12bfbbe0830d29d44dfc369
ywon0112 <yaewon.kim@ucsf.edu>
2022-06-27 14:13:56 -0700

    Update noise_est1_callback.m

--- Interactive_Denoising/callbackfun/noise_est1_callback.m
+++ Interactive_Denoising/callbackfun/noise_est1_callback.m
@@ -6,7 +6,7 @@ function noise_est1_callback(h_obj, event)
     global window
     global asnr
     
-[snr asnr noise_avg raw_noise_std ndata noisemask] = noise_est1(permute(im,[1 2 4 3]), 0);
+[snr asnr noise_avg raw_noise_std ndata noisemask] = noise_est1(permute(im,[1 2 4 3]),0);
 % p_noise_cal_result = [0.33 0.40 0.1 0.03]; 
 p_noise_cal_result = [0.02 0.09 0.3 0.03]   ;
 
