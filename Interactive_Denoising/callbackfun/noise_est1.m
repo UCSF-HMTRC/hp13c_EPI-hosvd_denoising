df718439afa47cac98aaf834a8a0eb3e367995a7
ywon0112 <yaewon.kim@ucsf.edu>
2022-06-27 14:13:52 -0700

    Update noise_est1.m

--- Interactive_Denoising/callbackfun/noise_est1.m
+++ Interactive_Denoising/callbackfun/noise_est1.m
@@ -1,4 +1,4 @@
-function [snr asnr noise_avg noise_std ndata noise_mask] =  noise_est1_callback(data, dn, mask)
+function [snr asnr noise_avg noise_std ndata noise_mask] =  noise_est1(data, dn, mask)
 
     X = size(data,2); Y = size(data,1);
     num_frames = size(data,3);
@@ -8,11 +8,12 @@ function [snr asnr noise_avg noise_std ndata noise_mask] =  noise_est1_callback(
     if nargin < 3
         % AUC images
         hh=figure; imagesc(auc); axis off; colormap default    
-        title('Draw an signal ROI to exclude in noise estimation');
+        title('Draw an signal ROI to exclude in noise estimation and hit enter');
         p = drawfreehand('LineWidth',2,'Color','cyan');
         pause
         %     
         background= ones(Y,X) - createMask(p);
+       
         close(hh) 
         noise_mask = ones(Y,X) .* background;
         noise_mask(:,1,:) = 0;
@@ -36,4 +37,5 @@ function [snr asnr noise_avg noise_std ndata noise_mask] =  noise_est1_callback(
         snr = data/noise_std;
         asnr = sum(data ,3)/(sqrt(num_frames)*noise_std);
         ndata = data;
+    end
 end
